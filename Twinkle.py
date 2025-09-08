7febd3386cc745b1fa3ad7e5746321b93e4353e1
JacobHino <jacobfbman@hotmail.com>
2025-09-08 09:18:37 -0500

    Update Twinkle.py

--- Twinkle.py
+++ Twinkle.py
@@ -5,3 +5,5 @@ print("\tUp above the world so high,")
 print("\tLike a diamond in the sky")
 print("Twinkle,", "Twinkle,", "little star", sep=" ")
 print("\tHow I wonder what you are")
+
+print("\Rudolph the red nosed reindeer")
