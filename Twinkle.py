d93140ff8d7442678a31eba834811ee9fd587c19
Mrdqrk <126504795+Mrdqrk@users.noreply.github.com>
2025-09-26 09:14:26 -0500

    Delete Twinkle.py

--- Twinkle.py
+++
@@ -1,9 +0,0 @@
-
-print("Twinkle,", "Twinkle,", "little star", sep=" ")
-print("\tHow I wonder what you are")
-print("\tUp above the world so high,")
-print("\tLike a diamond in the sky")
-print("Twinkle,", "Twinkle,", "little star", sep=" ")
-print("\tHow I wonder what you are")
-
-print("testing")
