f507e794b569ab3cdb026f97b056a97b2575108b
Bread629 <gpipes1@leomail.tamuc.edu>
2025-09-29 07:49:14 -0700

    Update AI_Agent.py

--- AI_Agent.py
+++ AI_Agent.py
@@ -2,7 +2,7 @@ import string
 import random
 
 def hangman_agent(guessed_letters, revealed_phrase, word):
-    alphabet = set(string.ascii_lowercase)
+    alphabet = set(string.ascii_uppercase)
     remaining = list(alphabet - set(guessed_letters))
     if remaining:
         return random.choice(remaining)
