22f3218072512c8b684fcdb6137c280ea1d3d09c
Joshua Weilbacher <joshuaw2017@gmail.com>
2025-12-07 20:26:08 -0600

    final security additions and works with game set up.

--- security.py
+++ security.py
@@ -5,11 +5,35 @@ from dotenv import load_dotenv
 
 
 def load_secrets():
-    load_dotenv()
+    load_dotenv("secret.env")
     return {
         "SUPABASE_URL": os.getenv("SUPABASE_URL"),
         "SUPABASE_KEY": os.getenv("SUPABASE_KEY"),
     }
+
+def create_user(username, password):
+    from supabase_client import db
+    hashed = hash_text(password)
+    db.client.from_("players").insert({
+        "playername": username,
+        "password_hash": hashed.decode()
+    }).execute()
+
+def authenticate(username, password):
+    from supabase_client import db
+    result = db.client.from_("players").select("*").eq("playername", username).execute()
+    
+    # no such user
+    if not result.data:
+        return False
+
+    row = result.data[0]
+
+    if "password_hash" not in row or not row["password_hash"]:
+        return False
+
+    stored_hash = row["password_hash"].encode()
+    return verify_text(password, stored_hash)
 
 
 def hash_text(text: str) -> bytes:
